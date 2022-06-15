0b96574ad0af7062bdb0edb217e133cd30f1a95d
mneus <maxence.neus@polytech-lille.fr>
2022-06-15 16:19:06 +0200

    Launch check on an interval

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,9 @@
-chrome.runtime.onInstalled.addListener(()=> {
-	chrome.storage.sync.set({ color });
-	console.log('Default background color set to %cgreen', `color: ${color}`);
-});
-
-chrome.tabs.onActivated.addListener((tab) => {
+/*var dv = document.createElement('div');
+dv.id = 'myid';
+dv.className = 'ad-boxes'
+dv.innerHTML = 'test';
+*/
+chrome.tabs.onCreated.addListener((tab) => {
   	chrome.scripting.executeScript({
     		target: { tabId: tab.tabId },
     		files: ["content-script.js"]
@@ -13,3 +13,19 @@ chrome.tabs.onActivated.addListener((tab) => {
 chrome.runtime.onMessage.addListener((message, sender) => {
 	console.log(message.className + ": " + message.visibility);
 });
+
+setInterval(() => {
+	chrome.tabs.query({active: true}, (res) => {
+		for (const tab of res) {
+			chrome.scripting.executeScript({
+    				target: { tabId: tab.id },
+    				files: ["content-script.js"]
+ 			});
+			console.log(tab.title);
+			chrome.tabs.sendMessage(tab.id, {className: 'ad-boxes'}, (response) => {
+				console.log('ad-boxes: ' + response.display);
+			});
+		}
+	});
+
+}, 2000);
--- extension/content-script.js
+++ extension/content-script.js
@@ -1,17 +1,27 @@
 function sleep(time) {
 	return new Promise((r) => setTimeout(r, time));
 }
-
+/*
 var dv = document.createElement('div');
 dv.id = 'myid';
 dv.className = 'ad-boxes'
 dv.innerHTML = 'test';
-document.body.appendChild(dv);
+*/
+chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+	var dv = document.createElement('div');
+	dv.id = 'myid';
+	dv.className = req.className
+	dv.innerHTML = 'test';
 
-const style = getComputedStyle(dv, null);
+	document.body.appendChild(dv);
+	const style = getComputedStyle(dv, null);
+	sendResponse({display: style.display});
+	document.body.removeChild(dv);
+});
 
+
+/*
 chrome.runtime.sendMessage({
 	className: "ad-boxes",
 	visibility: style.display
-})
-document.body.removeChild(dv);
+})*/
