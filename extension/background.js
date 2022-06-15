7083509ed749fe7fa70ff48b076e44b7ed10d12c
mneus <maxence.neus@polytech-lille.fr>
2022-06-15 10:47:48 +0200

    Message back display state

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,15 @@
-let color = '#3aa757'
-
 chrome.runtime.onInstalled.addListener(()=> {
 	chrome.storage.sync.set({ color });
 	console.log('Default background color set to %cgreen', `color: ${color}`);
 });
 
 chrome.tabs.onActivated.addListener((tab) => {
-  console.log("trying");
-  chrome.scripting.executeScript({
-    target: { tabId: tab.tabId },
-    files: ["content-script.js"]
-  });
+  	chrome.scripting.executeScript({
+    		target: { tabId: tab.tabId },
+    		files: ["content-script.js"]
+ 	});
+});
+
+chrome.runtime.onMessage.addListener((message, sender) => {
+	console.log(message.className + ": " + message.visibility);
 });
--- extension/content-script.js
+++ extension/content-script.js
@@ -1,5 +1,17 @@
+function sleep(time) {
+	return new Promise((r) => setTimeout(r, time));
+}
+
 var dv = document.createElement('div');
 dv.id = 'myid';
 dv.className = 'ad-boxes'
 dv.innerHTML = 'test';
 document.body.appendChild(dv);
+
+const style = getComputedStyle(dv, null);
+
+chrome.runtime.sendMessage({
+	className: "ad-boxes",
+	visibility: style.display
+})
+document.body.removeChild(dv);
