2dc9fc6ef1c7629f685e1aacc89f5b4fa15c6e02
mneus <maxence.neus@polytech-lille.fr>
2022-06-16 15:41:45 +0200

    Create Get endpoint for list of classes

---
+++ extension/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+var app = express();
+app.use(cors());
+
+const rules = {classNames: ["ad-boxes", "hey"]};
+
+app.get('/rules', (req, res) => {
+	res.send(rules);
+});
+
+app.listen(8080, () => {
+	console.log("Running on localhost:8080");
+});
