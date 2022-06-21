66692ea972f2e5d16fce2cc737a0a180a199f9a4
mneus <maxence.neus@polytech-lille.fr>
2022-06-21 16:15:57 +0200

    Day 13

--- extension/content2.js
+++ extension/content2.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		res.json().then(res => {
 			res.classNames.map((className) => {
 				var dv = document.createElement('div');
-				dv.innerHTML = "<div id=" + className + /*" src='http://www.example.com/?ad=true'*/">test</div>";
+				dv.innerHTML = "<img id=" + className + " src='http://localhost:8080/rules'>test</div>";
 				var elem = dv.firstChild;
 				document.body.appendChild(elem);	
 				setTimeout(() => {
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
 									className: className, 
 									visibility: getComputedStyle(elem, null).display
 					}, () => {
-						//document.body.removeChild(elem);
-						return 0;
+						document.body.removeChild(elem);
+						//return 0;
 					});
 				}, 10000);
 			});
