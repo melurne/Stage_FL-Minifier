67ca03b4fec3e76723feb5d68bd89735406c9a23
mneus <maxence.neus@polytech-lille.fr>
2022-08-02 16:00:39 +0200

    extension: crappy fix for wierd uBO bug

--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let el = document.createElement('div')
+    document.body.appendChild(el);
+    console.log(getComputedStyle(el, null).display)
     setTimeout(() => {
         fetch("http://localhost:8080/tests").then((res) => {
             res.json().then(res => {
@@ -9,6 +12,37 @@ document.addEventListener('DOMContentLoaded', () => {
                     dv.innerHTML = test;
                     var elem = dv.firstChild;
                     document.body.appendChild(elem);
+                    console.log(getComputedStyle(elem, null).display)
+                    results.push(getComputedStyle(elem, null).display == "none");
+                }
+                console.log(results); 
+                // fetch("http://localhost:8080/result", {
+                //     method: 'POST',
+                //     headers: {
+                //         'Accept': 'application/json',
+                //         'Content-Type': 'application/json'
+                //     },
+                //     body: JSON.stringify({id:"extension", results: results})
+                // }).then(() => {
+                //     console.log("Data posted");
+                // });
+            });
+        });
+    }, 1000);
+
+    // When we change the custom rules in uBO, the first time we run a test always return all falses
+    
+    setTimeout(() => {
+        fetch("http://localhost:8080/tests").then((res) => {
+            res.json().then(res => {
+                console.log(res[0].tests)
+                results = [];
+                for (test of res[0].tests) {
+                    var dv = document.createElement('div');
+                    dv.innerHTML = test;
+                    var elem = dv.firstChild;
+                    document.body.appendChild(elem);
+                    console.log(getComputedStyle(elem, null).display)
                     results.push(getComputedStyle(elem, null).display == "none");
                 }
                 console.log(results); 
@@ -24,5 +58,5 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
             });
         });
-    }, 10);
+    }, 2000);
 });
