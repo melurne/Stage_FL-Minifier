5a865464a4cbf9431dac4b42357f48877cdb2ec2
mneus <maxence.neus@polytech-lille.fr>
2022-08-03 10:26:14 +0200

    extension: Remove elements after test

--- extension/content.js
+++ extension/content.js
@@ -1,49 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let el = document.createElement('div')
-    document.body.appendChild(el);
-    console.log(getComputedStyle(el, null).display)
     setTimeout(() => {
         fetch("http://localhost:8080/tests").then((res) => {
             res.json().then(res => {
-                console.log(res[0].tests)
                 results = [];
                 for (test of res[0].tests) {
                     var dv = document.createElement('div');
                     dv.innerHTML = test;
                     var elem = dv.firstChild;
                     document.body.appendChild(elem);
-                    console.log(getComputedStyle(elem, null).display)
                     results.push(getComputedStyle(elem, null).display == "none");
+                    document.body.removeChild(elem);
                 }
-                console.log(results); 
-                // fetch("http://localhost:8080/result", {
-                //     method: 'POST',
-                //     headers: {
-                //         'Accept': 'application/json',
-                //         'Content-Type': 'application/json'
-                //     },
-                //     body: JSON.stringify({id:"extension", results: results})
-                // }).then(() => {
-                //     console.log("Data posted");
-                // });
             });
         });
-    }, 1000);
+    }, 10);
 
     // When we change the custom rules in uBO, the first time we run a test always return all falses
     
     setTimeout(() => {
         fetch("http://localhost:8080/tests").then((res) => {
-            res.json().then(res => {
-                console.log(res[0].tests)
-                results = [];
-                for (test of res[0].tests) {
+            res.json().then(elems => {
+                imgs = elems[0].tests.map(t => {
                     var dv = document.createElement('div');
-                    dv.innerHTML = test;
-                    var elem = dv.firstChild;
+                    dv.innerHTML = t;
+                    return dv.firstChild;
+                });
+                results = [];
+                for (elem of imgs) {
                     document.body.appendChild(elem);
-                    console.log(getComputedStyle(elem, null).display)
                     results.push(getComputedStyle(elem, null).display == "none");
+                    document.body.removeChild(elem);
                 }
                 console.log(results); 
                 fetch("http://localhost:8080/result", {
@@ -54,9 +40,9 @@ document.addEventListener('DOMContentLoaded', () => {
                     },
                     body: JSON.stringify({id:"extension", results: results})
                 }).then(() => {
-                    console.log("Data posted");
+                    console.log("Data Logged");
                 });
             });
         });
-    }, 2000);
+    }, 100);
 });
