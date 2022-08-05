12eb56ee1a738718bc76d2ecd8b1342774f7e8df
mneus <maxence.neus@polytech-lille.fr>
2022-08-05 15:59:52 +0200

    extension: history fetch data

--- extension/AnalyticsWindow/analytics.js
+++ extension/AnalyticsWindow/analytics.js
@@ -1,6 +1,27 @@
-// id = "extension"
-// fetch("http://localhost:8080/analytics/" + id).then(raw => {
-//     raw.json().then(data => {
+id = "extension"
 
-//     });
-// });
+let reportsContainer = document.getElementsByClassName("reportsList")[0];
+let timeline = document.getElementsByClassName("timeline")[0];
+
+
+fetch("http://localhost:8080/analytics/" + id).then(raw => {
+    raw.json().then(data => {
+        for (date in data) {
+            container = document.createElement('span');
+            container.className = "report";
+
+            detected = document.createElement('span')
+            detected.className = "detected"
+            container.appendChild(detected);
+
+            detectedSubtitle = document.createElement('div');
+            detectedSubtitle.className = "report-subtitle";
+            detectedSubtitle.innerHTML = data[date]["state"].length + " Lists detected";
+            detected.appendChild(detectedSubtitle);
+
+            reportsContainer.appendChild(container);
+
+            // console.log(container);
+        }
+    });
+});
